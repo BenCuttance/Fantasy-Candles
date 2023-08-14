1049becdb0d6f8f62fbd9fddc918c3d30ff5614a
BenC <bcuttance1998@gmail.com>
2023-08-14 19:35:04 +0930

    added server side folder. Added Seeds with started names

---
+++ Server/models/index.js
@@ -0,0 +1,5 @@
+const Tech = require('./Tech');
+const Matchup = require('./Candle');
+const User = require("./User");
+
+module.exports = { Tech, Matchup, User };
---
+++ Server/schemas/typeDefs.js
@@ -0,0 +1,43 @@
+const { gql } = require("apollo-server-express");
+const typeDefs = gql`
+  type Candle {
+    candleId: String!
+    candleTitle: String!
+    candleSize: String!
+    candleColor: String!
+    candleScent: String!
+    candlePrice: String!
+    candleDescription: String!
+    candleImage: String!
+  }
+  type User {
+    _id: ID
+    username: String
+    email: String
+    savedCandles: [Candle]!
+  }
+  type Auth {
+    token: ID!
+    user: User
+  }
+  input CandleInput {
+    candleId: String!
+    candleTitle: String!
+    candleSize: String!
+    candleColor: String!
+    candleScent: String!
+    candlePrice: String!
+    candleDescription: String!
+    candleImage: String!
+  }
+  type Query {
+    me: User
+  }
+  type Mutation {
+    addUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+    saveCandle(candle: CandleInput): User
+    removeCandle(candleId: String): User
+  }
+`;
+module.exports = typeDefs;
