741821465af504145ef05a54828e155bd26afa4c
Robinson Dos Santos <robinsonfdossantos@gmail.com>
2023-08-14 22:16:20 +0930

    Start creating Login and Signup pages

---
+++ src/utils/localStorage.js
@@ -0,0 +1,30 @@
+export const getSavedCandleIds = () => {
+  const savedCandleIds = localStorage.getItem('saved_candles')
+    ? JSON.parse(localStorage.getItem('saved_candles'))
+    : [];
+
+  return savedCandleIds;
+};
+
+export const saveCandleIds = (candleIdArr) => {
+  if (candleIdArr.length) {
+    localStorage.setItem('saved_candles', JSON.stringify(candleIdArr));
+  } else {
+    localStorage.removeItem('saved_candles');
+  }
+};
+
+export const removeCandleId = (candleId) => {
+  const savedCandleIds = localStorage.getItem('saved_candles')
+    ? JSON.parse(localStorage.getItem('saved_candles'))
+    : null;
+
+  if (!savedCandleIds) {
+    return false;
+  }
+
+  const updatedSavedCandleIds = savedCandleIds?.filter((savedCandleId) => savedCandleId !== candleId);
+  localStorage.setItem('saved_candles', JSON.stringify(updatedSavedCandleIds));
+
+  return true;
+};
