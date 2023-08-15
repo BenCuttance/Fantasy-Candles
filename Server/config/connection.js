66781d5fdf2399f0f8f2ae8157c45aaad526a32f
Natasa <nkilibarda3@gmail.com>
2023-08-15 17:19:49 +1000

    delete Server

--- Server/config/connection.js
+++
@@ -1,5 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/storecandles');
-
-module.exports = mongoose.connection;
--- Server/models/index.js
+++
@@ -1,5 +0,0 @@
-const Tech = require('./Tech');
-const Matchup = require('./Candle');
-const User = require("./User");
-
-module.exports = { Tech, Matchup, User };
